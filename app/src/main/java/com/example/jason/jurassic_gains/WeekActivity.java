923794835a0333e5acc2266f695164849d2e4ec8
Unknown <jmmorris2@mail.bradley.edu>
2018-06-27 11:52:53 -0500

    Add checkboxes

--- app/src/main/java/com/example/jason/jurassic_gains/WeekActivity.java
+++ app/src/main/java/com/example/jason/jurassic_gains/WeekActivity.java
@@ -1,43 +1,51 @@
 package com.example.jason.jurassic_gains;
 
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 
 public class WeekActivity extends AppCompatActivity {
 
-    private LinearLayout mLinearLayout;
-    private LayoutParams mButtonLayoutParams;
+    LinearLayout mLinearLayout;
+    LayoutParams weekLayoutParams;
+    LayoutParams checkBoxLayoutParams;
+    LayoutParams buttonLayoutParams;
+    int weeksNum = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week);
         mLinearLayout = findViewById(R.id.week_layout);
-        mButtonLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+
+        weekLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, 0, 1);
+        checkBoxLayoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.FILL_PARENT);
+        buttonLayoutParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
 
 
-        for (int i = 1; i < 13; i++) {
+        for (int i = 1; i <= weeksNum; i++) {
 
-            // Create a button for each week.
+            LinearLayout weekLayout = new LinearLayout(getApplicationContext());
+            weekLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+            CheckBox checkBox = new CheckBox(getApplicationContext());
+            checkBox.setGravity(Gravity.CENTER);
+            checkBox.setScaleX((float)1.3);
+            checkBox.setScaleY((float)1.3);
+            weekLayout.addView(checkBox, checkBoxLayoutParams);
+
             Button button = new Button(getApplicationContext());
             button.setTextSize(30);
-            button.setGravity(Gravity.START);
+            button.setGravity(Gravity.CENTER);
             button.setText("Week " + String.valueOf(i));
-            // button.setOnClickListener(view -> setMatchup(awayTeam, homeTeam, mLeague, teamAbbreviation));
+            weekLayout.addView(button, buttonLayoutParams);
 
-            // Add the button to the layout.
-            mLinearLayout.addView(button, mButtonLayoutParams);
+            mLinearLayout.addView(weekLayout, weekLayoutParams);
         }
 
     }
