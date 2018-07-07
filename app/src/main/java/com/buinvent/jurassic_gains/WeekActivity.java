34186c6f589fe9ffb61bfb160af7dbc25677807e
Unknown <jmmorris2@mail.bradley.edu>
2018-07-07 13:54:38 -0500

    ListView experience

--- app/src/main/java/com/buinvent/jurassic_gains/WeekActivity.java
+++ app/src/main/java/com/buinvent/jurassic_gains/WeekActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.content.SharedPreferences;
 import android.content.Context;
+import android.widget.ListView;
 
 
 // Activity for the screen that gives users the ability to select the week they're training
@@ -25,65 +27,13 @@ public class WeekActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week);
 
-        LinearLayout mLinearLayout = findViewById(R.id.week_layout);
         weekPreferences = getSharedPreferences(WEEK_PREFERENCES, Context.MODE_PRIVATE);
 
-        LayoutParams weekLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, 0, 1);
-        LayoutParams checkBoxLayoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.FILL_PARENT);
-        LayoutParams buttonLayoutParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+        String[] weekArray = {"WEEK 1", "WEEK 2", "WEEK 3", "WEEK 4", "WEEK 5"};
+        ArrayAdapter weekBTNAdapter = new ArrayAdapter<>(this, R.layout.activity_week_listview, weekArray);
 
-        // Declare and Initialize checkbox and button variables to be size of the number of weeks
-        CheckBox[] checkBoxes = new CheckBox[WEEKS_NUM];
-        Button[] buttons = new Button[WEEKS_NUM];
-
-        for (int i = 0; i < WEEKS_NUM; i++) {
-
-            LinearLayout weekLayout = new LinearLayout(this);
-            weekLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-            // Add checkbox for each week
-            checkBoxes[i] = new CheckBox(this);
-            checkBoxes[i].setGravity(Gravity.CENTER);
-            checkBoxes[i].setScaleX((float)1.3);
-            checkBoxes[i].setScaleY((float)1.3);
-            weekLayout.addView(checkBoxes[i], checkBoxLayoutParams);
-
-            // Set the checkbox to be checked or unchecked based on what was previously saved
-            checkBoxes[i].setChecked(weekPreferences.getBoolean("checkbox_week" + Integer.toString(i+1), false));
-
-            // Add a button and set the text for each week
-            buttons[i] = new Button(this);
-            buttons[i].setTextSize(30);
-            buttons[i].setGravity(Gravity.CENTER);
-            String weekText = "WEEK " + String.valueOf(i + 1);
-            buttons[i].setText(weekText);
-            // Set a click listener that will start the Day activity and send the week's text
-            buttons[i].setOnClickListener(view -> setDay(weekText));
-            weekLayout.addView(buttons[i],buttonLayoutParams);
-
-            // Do a checkbox listener if i > 1. This is because you need three checkboxes to run
-            // this function (one before, one middle, and one after)
-            if(i > 1) {
-                checkBoxListener(i, checkBoxes[i-1], checkBoxes[i-2], checkBoxes[i], buttons[i]);
-            }
-
-            // If the previous checkbox wasn't checked, dis the checkbox and buttons
-            if (i != 0 && !checkBoxes[i-1].isChecked()) {
-                checkBoxes[i].setEnabled(false);
-                buttons[i].setEnabled(false);
-            }
-
-            // Disable the previous checkbox if the checkbox is checked
-            if (i != 0 && checkBoxes[i].isChecked())
-                checkBoxes[i-1].setEnabled(false);
-
-            mLinearLayout.addView(weekLayout, weekLayoutParams);
-        }
-
-        // Add checkbox listener for the first week's checkbox
-        checkBoxListener(checkBoxes[0], checkBoxes[1], buttons[1]);
-        // Add a checkbox listener for the last week's checkbox
-        checkBoxListener(checkBoxes[WEEKS_NUM-1], checkBoxes[WEEKS_NUM-2]);
+        ListView mListView = findViewById(R.id.week_layout);
+        mListView.setAdapter(weekBTNAdapter);
 
     }
 
