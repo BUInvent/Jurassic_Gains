be79fbad4f68269dd5c7bf4f94ca513a191aa1f6
Unknown <jmmorris2@mail.bradley.edu>
2018-12-16 21:42:27 -0600

    Add logout

--- app/src/main/java/com/buinvent/jurassic_gains/WeekActivity.java
+++ app/src/main/java/com/buinvent/jurassic_gains/WeekActivity.java
@@ -1,9 +1,12 @@
 package com.buinvent.jurassic_gains;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -11,6 +14,11 @@ import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.content.SharedPreferences;
 import android.content.Context;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 // Activity for the screen that gives users the ability to select the week they're training
@@ -44,12 +52,12 @@ public class WeekActivity extends AppCompatActivity {
             // Add checkbox for each week
             checkBoxes[i] = new CheckBox(this);
             checkBoxes[i].setGravity(Gravity.CENTER);
-            checkBoxes[i].setScaleX((float)1.3);
-            checkBoxes[i].setScaleY((float)1.3);
+            checkBoxes[i].setScaleX((float) 1.3);
+            checkBoxes[i].setScaleY((float) 1.3);
             weekLayout.addView(checkBoxes[i], checkBoxLayoutParams);
 
             // Set the checkbox to be checked or unchecked based on what was previously saved
-            checkBoxes[i].setChecked(weekPreferences.getBoolean("checkbox_week" + Integer.toString(i+1), false));
+            checkBoxes[i].setChecked(weekPreferences.getBoolean("checkbox_week" + Integer.toString(i + 1), false));
 
             // Add a button and set the text for each week
             buttons[i] = new Button(this);
@@ -59,23 +67,23 @@ public class WeekActivity extends AppCompatActivity {
             buttons[i].setText(weekText);
             // Set a click listener that will start the Day activity and send the week's text
             buttons[i].setOnClickListener(view -> setDay(weekText));
-            weekLayout.addView(buttons[i],buttonLayoutParams);
+            weekLayout.addView(buttons[i], buttonLayoutParams);
 
             // Do a checkbox listener if i > 1. This is because you need three checkboxes to run
             // this function (one before, one middle, and one after)
-            if(i > 1) {
-                checkBoxListener(i, checkBoxes[i-1], checkBoxes[i-2], checkBoxes[i], buttons[i]);
+            if (i > 1) {
+                checkBoxListener(i, checkBoxes[i - 1], checkBoxes[i - 2], checkBoxes[i], buttons[i]);
             }
 
             // If the previous checkbox wasn't checked, dis the checkbox and buttons
-            if (i != 0 && !checkBoxes[i-1].isChecked()) {
+            if (i != 0 && !checkBoxes[i - 1].isChecked()) {
                 checkBoxes[i].setEnabled(false);
                 buttons[i].setEnabled(false);
             }
 
             // Disable the previous checkbox if the checkbox is checked
             if (i != 0 && checkBoxes[i].isChecked())
-                checkBoxes[i-1].setEnabled(false);
+                checkBoxes[i - 1].setEnabled(false);
 
             mLinearLayout.addView(weekLayout, weekLayoutParams);
         }
@@ -83,15 +91,15 @@ public class WeekActivity extends AppCompatActivity {
         // Add checkbox listener for the first week's checkbox
         checkBoxListener(checkBoxes[0], checkBoxes[1], buttons[1]);
         // Add a checkbox listener for the last week's checkbox
-        checkBoxListener(checkBoxes[WEEKS_NUM-1], checkBoxes[WEEKS_NUM-2]);
+        checkBoxListener(checkBoxes[WEEKS_NUM - 1], checkBoxes[WEEKS_NUM - 2]);
 
     }
 
 
-//    This is for all weeks that are not first or last
+    //    This is for all weeks that are not first or last
     private void checkBoxListener(final int weekNum, final CheckBox currentCheckBox,
                                   final CheckBox previousCheckBox, final CheckBox nextCheckBox,
-                                  final Button nextButton){
+                                  final Button nextButton) {
 
         currentCheckBox.setOnClickListener((View v) -> {
             // Enable the next checkbox and button if the current checkbox gets checked, disable if vice versa
@@ -107,9 +115,9 @@ public class WeekActivity extends AppCompatActivity {
         });
     }
 
-//    This is for the first week since it doesn't have a previous week
+    //    This is for the first week since it doesn't have a previous week
     private void checkBoxListener(final CheckBox currentCheckBox,
-                                  final CheckBox nextCheckBox, final Button nextButton){
+                                  final CheckBox nextCheckBox, final Button nextButton) {
 
         currentCheckBox.setOnClickListener((View v) -> {
             nextCheckBox.setEnabled(currentCheckBox.isChecked());
@@ -121,8 +129,8 @@ public class WeekActivity extends AppCompatActivity {
         });
     }
 
-//    For the last week since it doesn't have a next week
-    private void checkBoxListener(final CheckBox currentCheckBox, final CheckBox previousCheckBox){
+    //    For the last week since it doesn't have a next week
+    private void checkBoxListener(final CheckBox currentCheckBox, final CheckBox previousCheckBox) {
 
         currentCheckBox.setOnClickListener((View v) -> {
             previousCheckBox.setEnabled(!currentCheckBox.isChecked());
@@ -134,10 +142,34 @@ public class WeekActivity extends AppCompatActivity {
     }
 
     // Function to start the Day Activity
-    private void setDay(final String week){
+    private void setDay(final String week) {
         Intent day = new Intent(getApplicationContext(), DayActivity.class);
         day.putExtra(DayActivity.EXTRA_WEEK, week); // Send the week so the day activity knows what week was selected
         startActivity(day);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.sign_out_button) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(WeekActivity.this, "Logged out successfully",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), AuthActivity.class));
+                        }
+                    });
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
