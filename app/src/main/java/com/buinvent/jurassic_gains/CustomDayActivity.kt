bcb14c04d71ca5a6a3109b238b206b424c59dcbc
Unknown <jmmorris2@mail.bradley.edu>
2018-12-30 14:00:26 -0600

    Add custom day activity layout

---
+++ app/src/main/java/com/buinvent/jurassic_gains/CustomDayActivity.kt
@@ -0,0 +1,15 @@
+package com.buinvent.jurassic_gains
+
+import android.os.Bundle
+import android.app.Activity
+
+import kotlinx.android.synthetic.main.activity_custom_day.*
+
+class CustomDayActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_day)
+    }
+
+}
